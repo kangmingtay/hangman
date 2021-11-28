e16920199f67653ec3b25b5ad0772a4665990b75
kangmingtay <kang.ming1996@gmail.com>
2021-11-28 17:12:57 +0800

    add test to check lowercase ascii letters in game.letters

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -4,12 +4,15 @@ defmodule HangmanImplGameTest do
 
   alias Hangman.Impl.Game
   test "new game" do
-    game = Game.new_game("random")
+    word = "random"
+    game = Game.new_game(word)
 
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
-    assert game.letters == ["r", "a", "n", "d", "o", "m"]
+    assert game.letters == word |> String.codepoints()
+    assert game.letters == word |> String.downcase(:ascii) |> String.codepoints()
+    assert Enum.join(game.letters) |> String.match?(~r/^[[:lower:]]+$/)
 
   end
 
