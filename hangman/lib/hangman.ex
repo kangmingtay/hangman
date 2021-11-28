a528f5c6dca752f4f6e2778c46494ea5bcac76b6
kangmingtay <kang.ming1996@gmail.com>
2021-11-28 17:27:23 +0800

    refactor hangman types

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,12 +1,12 @@
 defmodule Hangman do
 
-  alias Hangman.Impl.Game, as: Game
-  
-  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  alias Hangman.Impl.Game
+  alias Hangman.Type
+
   @opaque game :: Game.t 
   @type tally :: %{
     turns_left: integer, 
-    game_state: state,
+    game_state: Type.state,
     letters: list(String.t),
     used: list(String.t), 
   }
@@ -14,7 +14,7 @@ defmodule Hangman do
   @spec new_game() :: game
   defdelegate new_game, to: Game 
 
-  @spec make_move(game, String.t) :: { game, tally } 
+  @spec make_move(game, String.t) :: { game, Type.tally } 
   def make_move(_game, _guess) do
   end
 
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,9 +1,10 @@
 defmodule Hangman.Impl.Game do 
 
+  alias Hangman.Type
 
   @type t :: %__MODULE__{
     turns_left: integer,
-    game_state: Hangman.state,
+    game_state: Type.state,
     letters: list(String.t),
     used: MapSet.t(String.t),
   }
@@ -14,10 +15,12 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new(),
   ) 
 
+  @spec new_game() :: t 
   def new_game do 
     new_game(Dictionary.random_word())
   end
 
+  @spec new_game(String.t) :: t
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.codepoints
